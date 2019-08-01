40e72c6251c9e9602c61b5575ded0bf26971f296
MohitGupta2010 <50877026+MohitGupta2010@users.noreply.github.com>
2019-08-01 22:59:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prans/news/model/Contants.java
@@ -0,0 +1,37 @@
+package com.example.prans.news.model;
+
+/**
+ * Created by prans on 11-03-2018.
+ */
+
+public class Contants {
+
+
+    /********
+     * URLS
+     *******/
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+
+    //API Key
+    public static final String API_KEY = "545537c9a50245929830224f73afad45";
+
+    public static final String COUNTRY = "in";
+
+    public static final String SOURCES = "cnn," +
+            "the-new-york-times," +
+            "bbc-news," +
+            "the-guardian-uk," +
+            "google-news-au," +
+            "al-jazeera-english,";
+
+    public static final String CATEGORY_BUSINESS = "business";
+
+    public static final String CATEGORY_TECHNOLOGY = "technology";
+
+    public static final String CATEGORY_ENTERTAINMENT = "entertainment";
+
+    public static final String CATEGORY_SCIENCE = "science";
+
+    public static final String CATEGORY_SPORTS = "sports";
+
+}
